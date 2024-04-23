fe7929e059e4076568f047dd1a72fbc772bf7c02
Nick Green <nick.green@automattic.com>
2024-04-23 14:06:52 -0700

    Bump version

--- plugin-autoupdate-filter.php
+++ plugin-autoupdate-filter.php
@@ -3,7 +3,7 @@
 Plugin Name: Plugin Autoupdate Filter
 Plugin URI: https://github.com/a8cteam51/plugin-autoupdate-filter
 Description: Filters whether autoupdates are on based on day/time and other settings.
-Version: 1.5.2
+Version: 1.5.3
 Author: WordPress.com Special Projects
 Author URI: https://wpspecialprojects.wordpress.com/
 Update URI: https://github.com/a8cteam51/plugin-autoupdate-filter/
